5e7e666e911f642c31c251502125a44f9c40c66b
Adan <adangg1988@gmail.com>
2026-01-12 16:41:35 +0100

    feat(popup): redesign extension popup with premium dark theme

--- entrypoints/popup/main.tsx
+++ entrypoints/popup/main.tsx
@@ -1,10 +1,17 @@
 import './style.css'
+import { browser } from 'wxt/browser'
+
+const manifest = browser.runtime.getManifest()
+const version = manifest.version || '1.0.0'
 
 const root = document.getElementById('root')!
 root.innerHTML = `
   <div class="popup">
-    <div class="logo">MV</div>
-    <h1>Mediavida <span>Premium+</span></h1>
-    <p>Mejora tu experiencia</p>
+    <div class="logo-container">
+        <span class="mv">MV</span><span class="premium">PREMIUM</span>
+    </div>
+    <div class="info">
+        <span class="version">v${version}</span>
+    </div>
   </div>
 `
